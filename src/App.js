4f2cdd765f21672cdb3ae69de4deabc41b1874f6
unknown <https://github.com/0sipov>
2021-02-25 19:06:49 +0200

    create TransactionHistory module

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import './App.css';
 import userData from './user.json';
 import statisticalData from './statistical-data.json';
 import friends from './friends.json';
+import transactions from './transactions.json';
 import Profile from './components/Profile/Profile';
 import Statistics from './components/Statistics/Statistics';
 import FriendList from './components/FriendList/FriendList';
+import TransactionHistory from './components/TransactionHistory/TransactionHistory';
 
 function App() {
   return (
@@ -12,6 +14,7 @@ function App() {
       <Profile props={userData} />
       <Statistics title="Upload Stats" stats={statisticalData} />
       <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </>
   );
 }
